d4f60fc0ef827e1a415d5c44919bac16e4ac4494
Denis Araki <scaramouchedpa@pc.speedport.ip>
2019-03-09 17:58:27 +0100

    Added AboutUs container and minor fixes

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import './AboutUs.css';
+
 const aboutUs = () => (
-    <div>
-        <p>About Us</p>
+    <div className='AboutUs'>
+        <div className='AboutUsContent'>
+        <h3>About Us</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor quam nec massa ultricies, ac tincidunt orci gravida. Nullam sit amet diam risus. Curabitur nec velit suscipit eros tincidunt venenatis. Ut vel orci vitae enim varius
+                varius in vel neque. Fusce condimentum nibh vel neque egestas sagittis. Phasellus nec ex mauris. Mauris fringilla pulvinar augue. Vivamus vehicula dictum mi id sagittis. Curabitur vitae justo dignissim, scelerisque sapien non, iaculis
+                mauris. Donec id mauris augue. Morbi accumsan tristique imperdiet. Donec feugiat semper diam, a volutpat neque venenatis nec.</p>
+            <p>Fusce elit metus, ornare vel pellentesque sed, malesuada quis mi. Donec porttitor mi id scelerisque aliquet. Quisque et consectetur lorem. Nunc lobortis molestie sapien, eu tempor erat accumsan sed. Phasellus semper lacus sit amet lorem
+                tristique laoreet. Quisque laoreet sed nunc eu varius. Donec et tincidunt libero. Morbi posuere dolor sed enim volutpat, a feugiat tellus commodo. Nulla facilisi. Praesent euismod ultrices dui quis accumsan. Nulla aliquet magna porttitor
+                neque scelerisque, sed pharetra nulla placerat. Quisque dictum, metus ac pulvinar sagittis, lacus ligula sollicitudin nulla, fringilla congue eros augue vel orci. Cras dignissim velit in nisl varius, pellentesque bibendum orci consequat.
+                Suspendisse a risus faucibus, pellentesque urna ac, molestie nisl. In ultrices, lectus eget eleifend vulputate, lectus nisl bibendum purus, vel rhoncus odio magna ac est.</p>
+        </div>
     </div>
 );
 
